91573ea25a41fcfb69415d3b3419da21e610306d
Akshaan Kakar <akshaan@ucla.edu>
2014-01-24 20:23:00 -0800

    Started command execution

--- execute-command.c
+++ execute-command.c
@@ -2,9 +2,10 @@
 
 #include "command.h"
 #include "command-internals.h"
-
+#include <sys/types.h>
 #include <error.h>
-
+#include <unistd.h>
+#include <sys/wait.h>
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
@@ -14,11 +15,31 @@ command_status (command_t c)
   return c->status;
 }
 
+void execute_simple_command(command_t command)
+{
+	pid_t pid;
+
+	if((pid = fork()) < 0)
+		error(1,0,"Could not create new process");
+
+	if (pid == 0) {
+		if(execvp(command->u.word[0], command->u.word) < 0)
+	 		error(1,0,"Command execution failed");
+
+		}
+
+	else waitpid(pid,&command->status,0);
+
+}	
+
+
 void
 execute_command (command_t c, int time_travel)
 {
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+  //error (1, 0, "command execution not yet implemented");
+ 
+	execute_simple_command(c);
 }
