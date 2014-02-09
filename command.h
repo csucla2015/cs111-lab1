dc215afba56d1d3fc75d0253e1c420c10d5bd634
csucla2015 <meetbhagdev@ucla.edu>
2014-02-08 23:35:40 -0800

    Added the execute time travel declaration. we have to use it in main and thus the need

--- command.h
+++ command.h
@@ -1,3 +1,6 @@
+
+
+
 // UCLA CS 111 Lab 1 command interface
 
 typedef struct command *command_t;
@@ -15,6 +18,8 @@ command_t read_command_stream (command_stream_t stream);
 
 /* Print a command to stdout, for debugging.  */
 void print_command (command_t);
+/* Execute commands with time travel.  */
+command_t execute_time_travel (command_stream_t);
 
 /* Execute a command.  Use "time travel" if the integer flag is
    nonzero.  */
@@ -23,3 +28,7 @@ void execute_command (command_t, int);
 /* Return the exit status of a command, which must have previously been executed.
    Wait for the command, if it is not already finished.  */
 int command_status (command_t);
+
+
+
+
